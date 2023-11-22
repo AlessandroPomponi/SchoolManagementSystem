b318b2862fbc43e13d2919e5bf66fc2c95165cdf
AlessandroPomponi <6247184@edui.vaniercollege.qc.ca>
2023-11-22 12:48:21 -0500

    added Student class

---
+++ src/main/Java/org/yi/dto/Student.java
@@ -0,0 +1,16 @@
+package org.yi.dto;
+
+public class Student {
+    private String fname;
+    private Course[] courses;
+    private String id;
+    private int courseNum;
+    private int nextId;
+    private String lname;
+    private Department department;
+
+    @Override
+    public String toString() {
+
+    }
+}
