691606954166162736e4a010e72ac4f87d2ea480
AlessandroPomponi <6247184@edui.vaniercollege.qc.ca>
2023-11-22 12:50:51 -0500

    added Teacher class

---
+++ src/main/Java/org/yi/dto/Teacher.java
@@ -0,0 +1,13 @@
+package org.yi.dto;
+
+public class Teacher {
+    private String lname;
+    private String fname;
+    private Department department;
+    private String id;
+
+    @Override
+    public String toString() {
+
+    }
+}
