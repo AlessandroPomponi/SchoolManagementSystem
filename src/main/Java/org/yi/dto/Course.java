559bdf7a46e16e3f9d4ee1952883cb18a656f354
AlessandroPomponi <6247184@edui.vaniercollege.qc.ca>
2023-11-22 12:57:56 -0500

    added Course class

---
+++ src/main/Java/org/yi/dto/Course.java
@@ -0,0 +1,16 @@
+package org.yi.dto;
+
+public class Course {
+    private double credit;
+    private String id;
+    private Student[] students;
+    private Department department;
+    private int studentNum;
+    private Teacher teacher;
+    private String courseName;
+
+    @Override
+    public String toString() {
+
+    }
+}
