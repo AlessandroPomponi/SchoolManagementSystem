924612cce2eec0c532f43075df4274cb09caf94f
AlessandroPomponi <6247184@edui.vaniercollege.qc.ca>
2023-11-22 12:54:43 -0500

    added Department class

---
+++ src/main/Java/org/yi/dto/Department.java
@@ -0,0 +1,12 @@
+package org.yi.dto;
+
+public class Department {
+    private String id;
+    private int nextId;
+    private String departmentName;
+
+    @Override
+    public String toString() {
+
+    }
+}
