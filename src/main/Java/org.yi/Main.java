664cb963709504eb7726d89b35ca9961857f1570
AlessandroPomponi <6247184@edui.vaniercollege.qc.ca>
2023-11-22 16:57:17 -0500

    added parameters to Student class

--- src/main/Java/org.yi/Main.java
+++ src/main/Java/org.yi/Main.java
@@ -1,5 +1,10 @@
 package org.yi;
 
+/**
+ * Print Info
+ *
+ * @author Alessandro Pomponi
+ */
 public class Main {
     public static void main(String[] args) {
 
--- src/main/Java/org/yi/dto/Student.java
+++ src/main/Java/org/yi/dto/Student.java
@@ -4,6 +4,11 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * Information pertaining to students.
+ *
+ * @author Alessandro Pomponi
+ */
 @EqualsAndHashCode
 @Getter
 @Setter
@@ -16,8 +21,17 @@ public class Student {
     private String lname;
     private Department department;
 
+    public void Student(int nextId, String name, String fname, int courseNum, Department department, Course[] courses, String lname, String id) {
+        this.nextId = nextId;
+        this.name = fname + " " + lname;
+        this.fname = fname;
+        this.courseNum = courseNum;
+        this.department = department;
+        this.courses = courses;
+        this.lname = lname;
+        this.id = String.format("S%01", nextId++);
+    }
     @Override
     public String toString() {
-
     }
 }
