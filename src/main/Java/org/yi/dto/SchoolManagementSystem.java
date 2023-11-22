9be25c12cf8d684c7c41df15408428a3b3cfe40f
AlessandroPomponi <6247184@edui.vaniercollege.qc.ca>
2023-11-22 17:21:29 -0500

    added the empty methods

--- src/main/Java/org/yi/dto/SchoolManagementSystem.java
+++ src/main/Java/org/yi/dto/SchoolManagementSystem.java
@@ -8,4 +8,55 @@ import lombok.Setter;
 @Setter
 @Getter
 public class SchoolManagementSystem {
+    public Department findDepartment(String departmentName) {
+
+    }
+
+    public void printTeachers() {
+
+    }
+
+    public void modifyCourseTeacher(String courseName, String id) {
+
+    }
+
+    public void addDepartment(String departmentName) {
+
+    }
+
+    public void printStudents(String id) {
+
+    }
+
+    public Student findStudent(String id) {
+
+    }
+
+    public void addCourse(String, ) {
+
+    }
+
+    public void registerCourse() {
+
+    }
+
+    public void addTeacher() {
+
+    }
+
+    public Course findCourse() {
+
+    }
+
+    public void printDepartment() {
+
+    }
+
+    public void addStudent() {
+
+    }
+
+    public Teacher findTeahcer() {
+
+    }
 }
