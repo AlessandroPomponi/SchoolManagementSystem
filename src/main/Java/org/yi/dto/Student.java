ba480d08527a3f73b695d19cd765fa7bd39707d2
AlessandroPomponi <6247184@edui.vaniercollege.qc.ca>
2023-11-23 14:10:18 -0500

    added potential fix to Student class

--- src/main/Java/org/yi/dto/Student.java
+++ src/main/Java/org/yi/dto/Student.java
@@ -3,6 +3,7 @@ package org.yi.dto;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 /**
  * Information pertaining to students.
@@ -12,26 +13,20 @@ import lombok.Setter;
 @EqualsAndHashCode
 @Getter
 @Setter
+@ToString
 public class Student {
     private String fname;
     private Course[] courses;
     private String id;
     private int courseNum;
-    private int nextId;
+    private static int nextId = 1;
     private String lname;
     private Department department;
 
-    public void Student(int nextId, String name, String fname, int courseNum, Department department, Course[] courses, String lname, String id) {
-        this.nextId = nextId;
-        this.name = fname + " " + lname;
+    public Student(String fname, String lname, Department department) {
         this.fname = fname;
-        this.courseNum = courseNum;
-        this.department = department;
-        this.courses = courses;
         this.lname = lname;
+        this.department = department;
         this.id = String.format("S%01", nextId++);
-    }
-    @Override
-    public String toString() {
     }
 }
