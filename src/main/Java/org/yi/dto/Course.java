ac231e867de75e4dd0e26f3768033845fd5c290e
AlessandroPomponi <6247184@edui.vaniercollege.qc.ca>
2023-11-23 14:41:55 -0500

    added potential fix to Course class

--- src/main/Java/org/yi/dto/Course.java
+++ src/main/Java/org/yi/dto/Course.java
@@ -4,6 +4,11 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * Information pertaining to courses.
+ *
+ * @author Alessandro Pomponi
+ */
 @EqualsAndHashCode
 @Getter
 @Setter
@@ -15,7 +20,14 @@ public class Course {
     private int studentNum;
     private Teacher teacher;
     private String courseName;
+    private static int nextId = 1;
 
+    public Course(String courseName, double credit, Department department) {
+        this.courseName = courseName;
+        this.credit = credit;
+        this.department = department;
+        this.id = String.format("C%01", nextId++);
+    }
     @Override
     public String toString() {
 
