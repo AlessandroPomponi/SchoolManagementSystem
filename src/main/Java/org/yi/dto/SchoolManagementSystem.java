73095d47a897e80616885c40a412f23a123d29f1
AlessandroPomponi <6247184@edui.vaniercollege.qc.ca>
2023-11-23 13:50:00 -0500

    added maxs

--- src/main/Java/org/yi/dto/SchoolManagementSystem.java
+++ src/main/Java/org/yi/dto/SchoolManagementSystem.java
@@ -3,11 +3,27 @@ package org.yi.dto;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
+/**
+ * Information pertaining to the school system.
+ *
+ * @author Alessandro Pomponi
+ */
 @EqualsAndHashCode
 @Setter
 @Getter
+@ToString
 public class SchoolManagementSystem {
+    private static final int MAX_DEPARTMENT_NUM = 5;
+    private static final int MAX_STUDENT_NUM = 200;
+    private static final int MAX_STUDENT_COURSE_REGISTRATION = 5;
+    private static final int MAX_TEACHER_NUM = 20;
+    private static final int MAX_COURSE_NUM = 30;
+    private static final int MAX_REGISTRATION_NUM = 5;
+    public SchoolManagementSystem(String ) {
+
+    }
     public Department findDepartment(String departmentName) {
 
     }
