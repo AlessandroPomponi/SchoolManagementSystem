8568d06a1dbca5db09493981bafe124f85e0f7f5
AlessandroPomponi <6247184@edui.vaniercollege.qc.ca>
2023-11-25 03:05:13 -0500

    added potential fix to Course class

--- src/main/Java/org/yi/dto/SchoolManagementSystem.java
+++ src/main/Java/org/yi/dto/SchoolManagementSystem.java
@@ -16,6 +16,8 @@ import lombok.ToString;
 @ToString
 public class SchoolManagementSystem {
     private static final int MAX_DEPARTMENT_NUM = 5;
+    private int departmentNum = 0;
+    private Department[] departments;
     private static final int MAX_STUDENT_NUM = 200;
     private static final int MAX_STUDENT_COURSE_REGISTRATION = 5;
     private static final int MAX_TEACHER_NUM = 20;
@@ -24,55 +26,105 @@ public class SchoolManagementSystem {
     public SchoolManagementSystem(String ) {
 
     }
-    public Department findDepartment(String departmentName) {
 
-    }
+    /**
+     * The method searches for a department in the departments based on the id.
+     * @param  departmentId department id
+     * @return a department or null if the id does not match with anything
+     */
+    public Department findDepartment(String departmentId) {}
 
-    public void printTeachers() {
+    /**
+     * The method displays all teachers in a school, only display the teacher that is not null. And only display
+     * the names of course and department if there is any.
+     */
+    public void printTeachers() {}
 
-    }
+    /**
+     * The method assigns a teacher to a specific course, based on teacherId and courseId, if no teacher or
+     * course can be found with those ids, print a message.
+     * @param teacherId teacher id
+     * @param courseId course id
+     */
+    public void modifyCourseTeacher(String teacherId, String courseId) {}
 
-    public void modifyCourseTeacher(String courseName, String id) {
+    /**
+     * The method add a new department to the list of departments.
+     * @param departmentName
+     */
+    public void addDepartment(String departmentName) {}
 
-    }
+    /**
+     * The method displays all students in a school. It will only display the student that is not null and only display
+     * the names of courses and department if there is any.
+     */
+    public void printStudents() {}
 
-    public void addDepartment(String departmentName) {
+    /**
+     * The method searches for a student in the students based on a studentId.
+     * @param studentId student id
+     * @return a student or null if the studentId does not match with anything in the students
+     */
+    public Student findStudent(String studentId) {}
 
-    }
+    /**
+     * The method adds a new course to the list of courses in the system.
+     * @param courseName the course's name
+     * @param credits the amount of credits the course has
+     * @param departmentId department id
+     */
+    public void addCourse(String courseName, double credits, String departmentId) {}
 
-    public void printStudents(String id) {
+    /**
+     * The method registers a course for a student, based on studentId and courseId, which will check
+     * if the studentId or courseId does not match with anything, print a message.
+     * If the student has register max amount courses (5), print a message.
+     * If the course has be registered by the max number of students (5), print a message.
+     * If the course has be registered by the student already, it cannot be registered again, print a message.
+     * Else add the course to the student’s registered courses array, and also add the student to
+     * the course’s registered students array.
+     * @param studentId the student id
+     * @param courseId the course id
+     */
+    public void registerCourse(String studentId, String courseId) {}
 
-    }
+    /**
+     * The method adds a new teacher to the list of teachers in the system.
+     * @param fname teacher's first name
+     * @param lname teacher's last name
+     * @param studentId teacher's if
+     */
+    public void addTeacher(String fname, String lname, String studentId) {}
 
-    public Student findStudent(String id) {
+    /**
+     * The method searches for a course in the courses based on a courseId.
+     * @param courseId course id
+     * @return a course or null if the courseId does not match with anything in the courses
+     */
+    public Course findCourse(String courseId) {}
 
-    }
+    /**
+     * Display all departments in a school, only display the department that is not null.
+     */
+    public void printDepartment() {}
 
-    public void addCourse(String, ) {
+    /**
+     * The method adds a new student to the list of students in the system.
+     * @param fname student's first name
+     * @param lname student's last name
+     * @param studentId student's id
+     */
+    public void addStudent(String fname, String lname, String studentId) {}
 
-    }
+    /**
+     * The method searches for a teacher in the teachers based on the id.
+     * @param teacherId teacher id
+     * @returna a teacher or null if the teacherId does not match with anything in the teachers
+     */
+    public Teacher findTeacher(String teacherId) {}
 
-    public void registerCourse() {
-
-    }
-
-    public void addTeacher() {
-
-    }
-
-    public Course findCourse() {
-
-    }
-
-    public void printDepartment() {
-
-    }
-
-    public void addStudent() {
-
-    }
-
-    public Teacher findTeahcer() {
-
-    }
+    /**
+     * Display all courses in a school, only display the course that is not null.
+     */
+    public void printCourses() {}
 }
